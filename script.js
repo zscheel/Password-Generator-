46c2f6aef907d660906ba303ddff2a53156b5777
zscheel <zachary.scheel1@gmail.com>
2023-03-12 17:30:22 -0500

    added content variable that defines functions, and adds casses

--- script.js
+++ script.js
@@ -14,5 +14,23 @@ function writePassword() {
 
 }
 
+function content () {
+    length = prompt("How long do you want your password?");
+    if (length >=  8 && length <= 128) {
+        lowerCase = confirm("Do you want lowercase letters? (y or n)");
+        upperCase = confirm("Do you want uppercase letters? (y or n)");
+        number = confirm("Do you want to include numbers?");
+        special = confirm("Do you want special character?");
+    } else {
+        alert("Password length needs to be between 8 and 128 Characters")
+        content();
+    }
+
+    
+    if (!lowerCase && !upperCase && !number && !special) {
+        alert("You need to choose at least one criteria!");
+        content()
+    }
+}
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
